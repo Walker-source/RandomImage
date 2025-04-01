1cef0d184496969c4d491593c63567f3a95f7ce5
MaDoG <aliasgraphics@icloud.com>
2025-04-02 01:18:28 +0300

    Random image app

--- RandomImage/ViewController.swift
+++ RandomImage/ViewController.swift
@@ -7,16 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    @IBOutlet var imageView: UIImageView!
-    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+final class ViewController: UIViewController {
+    @IBOutlet private var imageView: UIImageView!
+    @IBOutlet private var activityIndicator: UIActivityIndicatorView!
+    
+    private let url = URL(string: "https://picsum.photos/200/300")!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        activityIndicator.startAnimating()
+        fetchImage()
     }
 
-    @IBAction func updateImageDidPressed() {
+    @IBAction private func updateImageDidPressed() {
+        fetchImage()
     }
-
+    
+    private func fetchImage() {
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data else {
+                return
+            }
+            
+            DispatchQueue.main.async {[weak self] in
+                guard let self else { return }
+                imageView.image = UIImage(data: data)
+                activityIndicator.stopAnimating()
+            }
+        }.resume()
+    }
 }
 
